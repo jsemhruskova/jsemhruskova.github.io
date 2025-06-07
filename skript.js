e5c3031910175c3702d8ab23b6bc5fd4e6b39219
Lucie Hrušková <119292993+jsemhruskova@users.noreply.github.com>
2025-06-07 23:26:03 +0200

    Delete skript.js

--- skript.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelectorAll('.tlacitko');
-
-btn.forEach(function(tlacitko){
-    tlacitko.addEventListener('click', function(){
-        // Pro každou dočasnou proměnnou tlačítko se nám nastavé event listener onClick
-        alert('Kliknul jsi na tlačítko!');
-    })
-})
